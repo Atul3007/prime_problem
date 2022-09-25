fb582891a6e00c8b1a1c7143a1337a83096190da
Atul3007 <atuldwivedi859@gmail.com>
2022-09-25 12:44:41 +0530

    advance prime program

--- prime.js
+++ prime.js
@@ -1,8 +1,10 @@
 let n=13;
 for(let i=1;i<=13;i++){
-if(n%i==0){
+for(let j=1;j<=i;j++){
+if(i%j==0){
 count++;
 }
-if(count==1){
-console.log("prime");
+}
+if(count==2){
+console.log(i,"prime number");
 }
